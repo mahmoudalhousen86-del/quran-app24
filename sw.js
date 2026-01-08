08f2475016dfe0e627d1b88e13df8512d615cdd5
mahmoudalhousen86-del <mahmoudalhousen86@gmail.com>
2026-01-08 10:24:35 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
-self.addEventListener('install', e => {
-    console.log('التطبيق مثبت');
+const CACHE_NAME = 'quran-app-full-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    'https://img.icons8.com/color/192/000000/quran.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
-self.addEventListener('fetch', e => {
-    console.log('يعمل بدون إنترنت');
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
